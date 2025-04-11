afb58f6069a0b408e75fa054af6c01d8cc3ebb64
Delta Student <aniket@2804>
2025-04-11 15:53:41 +0530

    New files

---
+++ script.js
@@ -0,0 +1,13 @@
+let n= 5;
+
+for (let i = 0; i<n; i++) {
+    console.log("hello,", i);
+}
+
+// console.log("bye!");
+
+// Process :- This Object Provides information about, and control over, the current Node.js process.
+
+// Process argv =
+
+console.log(process.argv);
